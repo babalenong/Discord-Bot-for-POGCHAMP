cab10ce9f95813d5564d224ae52b9158900ea321
babalenong <jotafauzanh@gmail.com>
2021-01-28 16:57:45 +0700

    added more pogs
    - pog
    - notpog
    - pogu
    - weirdchamp
    - adios (not pog)

--- index.js
+++ index.js
@@ -11,9 +11,26 @@ client.once('ready', () =>{
 
 client.on('message', message => {
 	if(message.author.bot) return;
+	// Lowercase to catch caps variants
 	const lowercase = message.content.toLowerCase();
+	// Word checks starts here
 	if(lowercase.includes('pogchamp')) {
-		message.channel.send('POGCHAMP', { files: ['https://raw.githubusercontent.com/babalenong/Discord-Bot-for-POGCHAMP/main/pogchamp1.png'] });
+		message.channel.send('POGCHAMP', { files: ['https://user-images.githubusercontent.com/13828964/106120010-6bfb2600-6188-11eb-99d3-e8f15fa91a52.png'] });
+	}
+	else if (lowercase.includes('pog\b')) {
+		message.channel.send('POG', { files: 'https://user-images.githubusercontent.com/13828964/106119997-6a316280-6188-11eb-9786-fde5c1294b3c.png' });
+	}
+	else if (lowercase.includes('notpog') || lowercase.includes('reverse pog') || lowercase.includes('not pog')) {
+		message.channel.send('that ain\'t pog', { files: 'https://user-images.githubusercontent.com/13828964/106119989-68679f00-6188-11eb-8b48-8d190ca00a98.png' });
+	}
+	else if (lowercase.includes('pogu\b')) {
+		message.channel.send('PogU', { files: 'https://user-images.githubusercontent.com/13828964/106120033-72899d80-6188-11eb-8a64-3f124164d0c1.png' });
+	}
+	else if (lowercase.includes('weirdchamp') || lowercase.includes('weird champ')) {
+		message.channel.send('WeirdChamp', { files: 'https://user-images.githubusercontent.com/13828964/106120035-74536100-6188-11eb-8f97-63029c571fba.png' });
+	}
+	else if (lowercase.includes('adios\b')) {
+		message.channel.send('Adios...', { files: 'https://media.tenor.co/videos/2b9443866424feea184db117fe54bacc/mp4' });
 	}
 });
 
